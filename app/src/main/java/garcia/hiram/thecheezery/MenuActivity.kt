a87eba6df53a5ca50bc387a3cc513080428a1f21
Hiram610 <hiram.garcia216629@potros.itson.edu.mx>
2023-02-09 00:00:20 -0700

    first commit

---
+++ app/src/main/java/garcia/hiram/thecheezery/MenuActivity.kt
@@ -0,0 +1,15 @@
+package garcia.hiram.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+
+    }
+}
